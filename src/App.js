92668b6f626f87a0e23355737e517bd795362f4d
Bryan O'Connor <oconnorjunkmail@gmail.com>
2021-11-22 09:06:02 -0700

    adds in initial commit for changing React default into my rebasing repo application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+			<div className="container-fluid p-0">
+				<div className="m-0 row">
+					<div className="col-12 p-0 text-center">
+						<h1 className="main-heading">
+							This is how we rebase.
+						</h1>
+					</div>
+					<div className="col-12 p-0 text-left">
+						<p className="xlg-paragraph lg-margin lg-padding">
+							Step 1: Make your multiple commits.
+						</p>
+						<p className="xlg-paragraph lg-margin lg-padding">
+							Step 2: $git rebase -i HEAD~&lt;number of commits&gt;.
+						</p>
+						<p className="xlg-paragraph lg-margin lg-padding">
+							Step 3: Squash desired commits (only do your non-public commits).
+						</p>
+						<p className="xlg-paragraph lg-margin lg-padding">
+							Step 4: Push up changes.
+						</p>
+
+						<h2 className="mt-5 sub-heading text-center">
+							Really funky way to see git log
+						</h2>
+						<p className="xlg-paragraph lg-margin lg-padding">
+							$git log --pretty=format:'%Cred%h%Creset -%C(blue)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
